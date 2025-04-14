a5a3dbe8b974d33383673792b9027ee897c69e1b
dj332 <devjariwala76@gmail.com>
2025-04-14 00:28:52 -0400

    fixes #7

--- tests/conftest.py
+++ tests/conftest.py
@@ -264,7 +264,7 @@ def user_response_data():
 
 @pytest.fixture
 def login_request_data():
-    return {"email": "john.doe@example.com", "password": "SecurePassword123!"}
+    return {"email": "john.doe@example.com", "password": "SecurePassword123!"} # changed username is email
 
 from datetime import timedelta
 # Fixes Issue #5
